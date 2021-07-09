75a5ed24501bd29d0f47a3a0a7fa967413c743ec
Jacob Tan <jtan052@bolt.cs.ucr.edu>
2021-07-09 16:39:05 -0700

    Iniital commit to branch

---
+++ typechecker/GjdVisitor.java
@@ -0,0 +1,629 @@
+package typechecker;
+
+import java.util.*;
+import syntaxtree.*;
+import visitor.*;
+
+public class GjdVisitor extends GJDepthFirst<HashMap<String, String>, HashMap<String, String> > {
+//
+   // User-generated visitor methods below
+   //
+
+   /**
+    * f0 -> MainClass()
+    * f1 -> ( TypeDeclaration() )*
+    * f2 -> <EOF>
+    */
+   public HashMap<String, String> visit(Goal n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "class"
+    * f1 -> Identifier()
+    * f2 -> "{"
+    * f3 -> "public"
+    * f4 -> "static"
+    * f5 -> "void"
+    * f6 -> "main"
+    * f7 -> "("
+    * f8 -> "String"
+    * f9 -> "["
+    * f10 -> "]"
+    * f11 -> Identifier()
+    * f12 -> ")"
+    * f13 -> "{"
+    * f14 -> ( VarDeclaration() )*
+    * f15 -> ( Statement() )*
+    * f16 -> "}"
+    * f17 -> "}"
+    */
+   public HashMap<String, String> visit(MainClass n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      n.f4.accept(this, argu);
+      n.f5.accept(this, argu);
+      n.f6.accept(this, argu);
+      n.f7.accept(this, argu);
+      n.f8.accept(this, argu);
+      n.f9.accept(this, argu);
+      n.f10.accept(this, argu);
+      n.f11.accept(this, argu);
+      n.f12.accept(this, argu);
+      n.f13.accept(this, argu);
+      n.f14.accept(this, argu);
+      n.f15.accept(this, argu);
+      n.f16.accept(this, argu);
+      n.f17.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> ClassDeclaration()
+    *       | ClassExtendsDeclaration()
+    */
+   public HashMap<String, String> visit(TypeDeclaration n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "class"
+    * f1 -> Identifier()
+    * f2 -> "{"
+    * f3 -> ( VarDeclaration() )*
+    * f4 -> ( MethodDeclaration() )*
+    * f5 -> "}"
+    */
+   public HashMap<String, String> visit(ClassDeclaration n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      n.f4.accept(this, argu);
+      n.f5.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "class"
+    * f1 -> Identifier()
+    * f2 -> "extends"
+    * f3 -> Identifier()
+    * f4 -> "{"
+    * f5 -> ( VarDeclaration() )*
+    * f6 -> ( MethodDeclaration() )*
+    * f7 -> "}"
+    */
+   public HashMap<String, String> visit(ClassExtendsDeclaration n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      n.f4.accept(this, argu);
+      n.f5.accept(this, argu);
+      n.f6.accept(this, argu);
+      n.f7.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> Type()
+    * f1 -> Identifier()
+    * f2 -> ";"
+    */
+   public HashMap<String, String> visit(VarDeclaration n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "public"
+    * f1 -> Type()
+    * f2 -> Identifier()
+    * f3 -> "("
+    * f4 -> ( FormalParameterList() )?
+    * f5 -> ")"
+    * f6 -> "{"
+    * f7 -> ( VarDeclaration() )*
+    * f8 -> ( Statement() )*
+    * f9 -> "return"
+    * f10 -> Expression()
+    * f11 -> ";"
+    * f12 -> "}"
+    */
+   public HashMap<String, String> visit(MethodDeclaration n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      n.f4.accept(this, argu);
+      n.f5.accept(this, argu);
+      n.f6.accept(this, argu);
+      n.f7.accept(this, argu);
+      n.f8.accept(this, argu);
+      n.f9.accept(this, argu);
+      n.f10.accept(this, argu);
+      n.f11.accept(this, argu);
+      n.f12.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> FormalParameter()
+    * f1 -> ( FormalParameterRest() )*
+    */
+   public HashMap<String, String> visit(FormalParameterList n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> Type()
+    * f1 -> Identifier()
+    */
+   public HashMap<String, String> visit(FormalParameter n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> ","
+    * f1 -> FormalParameter()
+    */
+   public HashMap<String, String> visit(FormalParameterRest n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> ArrayType()
+    *       | BooleanType()
+    *       | IntegerType()
+    *       | Identifier()
+    */
+   public HashMap<String, String> visit(Type n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "int"
+    * f1 -> "["
+    * f2 -> "]"
+    */
+   public HashMap<String, String> visit(ArrayType n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "boolean"
+    */
+   public HashMap<String, String> visit(BooleanType n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "int"
+    */
+   public HashMap<String, String> visit(IntegerType n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> Block()
+    *       | AssignmentStatement()
+    *       | ArrayAssignmentStatement()
+    *       | IfStatement()
+    *       | WhileStatement()
+    *       | PrintStatement()
+    */
+   public HashMap<String, String> visit(Statement n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "{"
+    * f1 -> ( Statement() )*
+    * f2 -> "}"
+    */
+   public HashMap<String, String> visit(Block n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> Identifier()
+    * f1 -> "="
+    * f2 -> Expression()
+    * f3 -> ";"
+    */
+   public HashMap<String, String> visit(AssignmentStatement n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> Identifier()
+    * f1 -> "["
+    * f2 -> Expression()
+    * f3 -> "]"
+    * f4 -> "="
+    * f5 -> Expression()
+    * f6 -> ";"
+    */
+   public HashMap<String, String> visit(ArrayAssignmentStatement n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      n.f4.accept(this, argu);
+      n.f5.accept(this, argu);
+      n.f6.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "if"
+    * f1 -> "("
+    * f2 -> Expression()
+    * f3 -> ")"
+    * f4 -> Statement()
+    * f5 -> "else"
+    * f6 -> Statement()
+    */
+   public HashMap<String, String> visit(IfStatement n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      n.f4.accept(this, argu);
+      n.f5.accept(this, argu);
+      n.f6.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "while"
+    * f1 -> "("
+    * f2 -> Expression()
+    * f3 -> ")"
+    * f4 -> Statement()
+    */
+   public HashMap<String, String> visit(WhileStatement n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      n.f4.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "System.out.println"
+    * f1 -> "("
+    * f2 -> Expression()
+    * f3 -> ")"
+    * f4 -> ";"
+    */
+   public HashMap<String, String> visit(PrintStatement n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      n.f4.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> AndExpression()
+    *       | CompareExpression()
+    *       | PlusExpression()
+    *       | MinusExpression()
+    *       | TimesExpression()
+    *       | ArrayLookup()
+    *       | ArrayLength()
+    *       | MessageSend()
+    *       | PrimaryExpression()
+    */
+   public HashMap<String, String> visit(Expression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> PrimaryExpression()
+    * f1 -> "&&"
+    * f2 -> PrimaryExpression()
+    */
+   public HashMap<String, String> visit(AndExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> PrimaryExpression()
+    * f1 -> "<"
+    * f2 -> PrimaryExpression()
+    */
+   public HashMap<String, String> visit(CompareExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> PrimaryExpression()
+    * f1 -> "+"
+    * f2 -> PrimaryExpression()
+    */
+   public HashMap<String, String> visit(PlusExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> PrimaryExpression()
+    * f1 -> "-"
+    * f2 -> PrimaryExpression()
+    */
+   public HashMap<String, String> visit(MinusExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> PrimaryExpression()
+    * f1 -> "*"
+    * f2 -> PrimaryExpression()
+    */
+   public HashMap<String, String> visit(TimesExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> PrimaryExpression()
+    * f1 -> "["
+    * f2 -> PrimaryExpression()
+    * f3 -> "]"
+    */
+   public HashMap<String, String> visit(ArrayLookup n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> PrimaryExpression()
+    * f1 -> "."
+    * f2 -> "length"
+    */
+   public HashMap<String, String> visit(ArrayLength n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> PrimaryExpression()
+    * f1 -> "."
+    * f2 -> Identifier()
+    * f3 -> "("
+    * f4 -> ( ExpressionList() )?
+    * f5 -> ")"
+    */
+   public HashMap<String, String> visit(MessageSend n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      n.f4.accept(this, argu);
+      n.f5.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> Expression()
+    * f1 -> ( ExpressionRest() )*
+    */
+   public HashMap<String, String> visit(ExpressionList n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> ","
+    * f1 -> Expression()
+    */
+   public HashMap<String, String> visit(ExpressionRest n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> IntegerLiteral()
+    *       | TrueLiteral()
+    *       | FalseLiteral()
+    *       | Identifier()
+    *       | ThisExpression()
+    *       | ArrayAllocationExpression()
+    *       | AllocationExpression()
+    *       | NotExpression()
+    *       | BracketExpression()
+    */
+   public HashMap<String, String> visit(PrimaryExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> <INTEGER_LITERAL>
+    */
+   public HashMap<String, String> visit(IntegerLiteral n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "true"
+    */
+   public HashMap<String, String> visit(TrueLiteral n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "false"
+    */
+   public HashMap<String, String> visit(FalseLiteral n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> <IDENTIFIER>
+    */
+   public HashMap<String, String> visit(Identifier n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "this"
+    */
+   public HashMap<String, String> visit(ThisExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "new"
+    * f1 -> "int"
+    * f2 -> "["
+    * f3 -> Expression()
+    * f4 -> "]"
+    */
+   public HashMap<String, String> visit(ArrayAllocationExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      n.f4.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "new"
+    * f1 -> Identifier()
+    * f2 -> "("
+    * f3 -> ")"
+    */
+   public HashMap<String, String> visit(AllocationExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      n.f3.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "!"
+    * f1 -> Expression()
+    */
+   public HashMap<String, String> visit(NotExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      return _ret;
+   }
+
+   /**
+    * f0 -> "("
+    * f1 -> Expression()
+    * f2 -> ")"
+    */
+   public HashMap<String, String> visit(BracketExpression n, HashMap<String, String> argu) {
+      HashMap<String, String> _ret=null;
+      n.f0.accept(this, argu);
+      n.f1.accept(this, argu);
+      n.f2.accept(this, argu);
+      return _ret;
+   }
+
+}
